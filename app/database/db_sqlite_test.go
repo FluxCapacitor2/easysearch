66febeb51b22974cd73d2f0da4b2172af773d234
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2024-08-23 16:15:33 -0400

    Add more tests

--- app/database/db_sqlite_test.go
+++ app/database/db_sqlite_test.go
@@ -1,16 +1,27 @@
 package database
 
 import (
+	"path"
 	"reflect"
 	"testing"
 )
 
-func TestSetup(t *testing.T) {
-	_, err := SQLite(":memory:")
+func createDB(t *testing.T) Database {
+	db, err := SQLite(path.Join(t.TempDir(), "temp.db"))
 
 	if err != nil {
 		t.Fatalf("database creation failed: %v", err)
 	}
+
+	if err := db.Setup(); err != nil {
+		t.Fatalf("database setup failed: %v", err)
+	}
+
+	return db
+}
+
+func TestSetup(t *testing.T) {
+	createDB(t)
 }
 
 func TestEscape(t *testing.T) {
@@ -33,15 +44,7 @@ func TestEscape(t *testing.T) {
 }
 
 func TestPopQueue(t *testing.T) {
-	db, err := SQLite(":memory:")
-
-	if err != nil {
-		t.Fatalf("database creation failed: %v", err)
-	}
-
-	if err := db.Setup(); err != nil {
-		t.Fatalf("database setup failed: %v", err)
-	}
+	db := createDB(t)
 
 	db.AddToQueue("source1", []string{"https://example.com/"}, 1)
 
@@ -73,15 +76,7 @@ func TestPopQueue(t *testing.T) {
 }
 
 func TestPopQueueWithOtherSource(t *testing.T) {
-	db, err := SQLite(":memory:")
-
-	if err != nil {
-		t.Fatalf("database creation failed: %v", err)
-	}
-
-	if err := db.Setup(); err != nil {
-		t.Fatalf("database setup failed: %v", err)
-	}
+	db := createDB(t)
 
 	db.AddToQueue("source1", []string{"https://example.com/"}, 1)
 
@@ -135,5 +130,77 @@ func TestProcessResultEmptyEnd(t *testing.T) {
 
 	if !reflect.DeepEqual(want, matches) {
 		t.Fatalf("wanted %+v, got %+v", want, matches)
+	}
+}
+
+func TestHasDocument(t *testing.T) {
+	db := createDB(t)
+
+	db.AddDocument("source1", 1, "https://example.com/", Finished, "Example Domain", "", "This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.")
+
+	res, err := db.HasDocument("source1", "https://example.com/")
+	if err != nil {
+		t.Fatalf("error fetching document: %v", err)
+	}
+	if !*res {
+		t.Fatalf("document was not added to database: hasDocument returned false")
+	}
+
+}
+
+func TestSearchQuery(t *testing.T) {
+	db := createDB(t)
+
+	err := db.AddDocument("source1", 1, "https://example.com/", Finished, "Example Domain", "", "This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.")
+	if err != nil {
+		t.Fatalf("error adding document: %v", err)
+	}
+
+	phrases := []struct {
+		phrase   string
+		expected int
+	}{
+		{phrase: "Example", expected: 1},
+		{phrase: "Example Domain", expected: 1},
+		{phrase: "\"Example Domain\"", expected: 1},
+		{phrase: "illustrative examp", expected: 1},
+		{phrase: "illustrative examples", expected: 1},
+		{phrase: "a_nonexistant_word", expected: 0},
+	}
+
+	for _, testCase := range phrases {
+		results, count, err := db.Search([]string{"source1"}, testCase.phrase, 1, 10)
+
+		if err != nil {
+			t.Fatalf("error fetching results for query '%v': %v", testCase.phrase, err)
+		}
+
+		if *count != uint32(testCase.expected) {
+			t.Fatalf("encountered unexpected result count: expected %v, got %v", testCase.expected, *count)
+		}
+
+		if len(results) != int(*count) {
+			t.Fatalf("'count' did not match length of results: %v != %v", *count, len(results))
+		}
+	}
+}
+
+func TestCleanQueue(t *testing.T) {
+	db := createDB(t)
+	db.AddToQueue("source1", []string{"https://example.com/1", "https://example.com/2"}, 1)
+	db.UpdateQueueEntry("source1", "https://example.com/1", Finished)
+	db.UpdateQueueEntry("source1", "https://example.com/2", Error)
+	{
+		err := db.CleanQueue()
+		if err != nil {
+			t.Fatalf("error cleaning queue: %v", err)
+		}
+	}
+	item, err := db.PopQueue("source1")
+	if err != nil {
+		t.Fatalf("error popping queue: %v", err)
+	}
+	if item != nil {
+		t.Fatalf("item was not removed from queue: %v", item)
 	}
 }
